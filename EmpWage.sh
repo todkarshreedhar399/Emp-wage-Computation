5c4b51934b06431771ac92122f3337ccfd0302c6
todkarshreedhar399 <todkarshreedhar399@gmail.com>
2021-07-27 01:19:27 +0530

    Latest Update 7

--- EmpWage.sh
+++ EmpWage.sh
@@ -7,21 +7,28 @@ isparttime=2
 emprateperhr=20
 totalemphrs=0
 totalworkingdays=0
+
+function workinghours() {
+         if [ $1 -eq $isfulltime ]
+   then
+      emphrs=8
+   elif [ $1 -eq $isparttime ]
+   then
+      emphrs=4
+   else
+      emphrs=0
+   fi
+        echo $emphrs
+}
+
 while [[ $totalemphrs -lt $maxhrs_month && $totalworkingdays -lt $workingDays ]]
 do
         ((totalworkingdays++))
         empcheck=$((RANDOM%3))
-        if [ $empcheck -eq $isfulltime ]
-        then
-                emphrs=8
-        elif [ $empcheck -eq $isparttime ]
-        then
-                emphrs=4
-        else
-                emphrs=0
-        fi
- wage=$(($emphrs*$emprateperhr))
-        totalemphrs=$(($emphrs+$totalemphrs))
+      
+ workhours="$( workinghours $empcheck )"
+        wage=$(($workhours*$emprateperhr))
+        totalemphrs=$(($workhours+$totalemphrs))
 done
  totalSalary=$(($emprateperhr*$totalemphrs))
-echo "Total Salary: $totalSalary"
+
